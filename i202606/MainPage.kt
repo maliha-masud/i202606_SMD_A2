ff4ff07dde13a3ad0fc70ee29d23a3ca24feff5a
Maliha Masud <i202606@nu.edu.pk>
2024-03-18 01:24:20 +0500

    Commit 5

--- i202606/MainPage.kt
+++ i202606/MainPage.kt
@@ -3,6 +3,12 @@ package com.maliha.i202606
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.widget.HorizontalScrollView
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.maliha.i202606.databinding.MainPgBinding
@@ -39,14 +45,65 @@ class MainPage : AppCompatActivity() {
                 })
         }
 
+        val topMentorsLinearLayout: LinearLayout = binding.topMentorsLinearLayout
+        val eduMentorsLinearLayout: LinearLayout = binding.eduMentorsLinearLayout
+        databaseReference.child("Mentors").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val totalMentors = dataSnapshot.childrenCount.toInt()
+                dataSnapshot.children.forEachIndexed { index, mentorSnapshot ->
+                    val mentor = mentorSnapshot.getValue(Mentor::class.java)
+                    if (mentor != null) {
+                        // Inflate the mentor item layout with the appropriate parent linear layout
+                        val parentLinearLayout = if (mentor.type == "Top") topMentorsLinearLayout else eduMentorsLinearLayout
+                        val mentorView = layoutInflater.inflate(R.layout.mentor_item, parentLinearLayout, false)
+
+                        // Populate the mentor view with mentor data
+                        mentorView.findViewById<TextView>(R.id.mentor_name_txt).text = mentor.name
+                        mentorView.findViewById<TextView>(R.id.mentor_desc).text = mentor.desc
+                        mentorView.findViewById<TextView>(R.id.mentor_price).text = "$" + mentor.price.toString() + "/Session"
+
+                        val mentorStatusTextView = mentorView.findViewById<TextView>(R.id.mentor_status_txt)
+                        mentorStatusTextView.text = mentor.status
+                        val mentorStatusRelativeLayout = mentorView.findViewById<RelativeLayout>(R.id.mentor_status)
+
+                        // Set text color based on mentor status
+                        if (mentor.status == "Available") {
+                            // Set color for Available status
+                            mentorStatusTextView.setTextColor(ContextCompat.getColor(this@MainPage, R.color.available_color))
+                            mentorStatusRelativeLayout.setBackgroundResource(R.drawable.available_circle)
+                        } else {
+                            // Set color for Not Available status
+                            mentorStatusTextView.setTextColor(ContextCompat.getColor(this@MainPage, R.color.not_available_color))
+                            mentorStatusRelativeLayout.setBackgroundResource(R.drawable.not_available_circle)
+                        }
+
+                        val favoriteButton = mentorView.findViewById<ImageButton>(R.id.fav_1)
+                        if (mentor.fav) {
+                            favoriteButton.setImageResource(R.drawable.filled_fav_heart)
+                        } else {
+                            favoriteButton.setImageResource(R.drawable.not_filled_fav_heart)
+                        }
+
+                        // Add the mentor view to the parent linear layout
+                        parentLinearLayout.addView(mentorView)
+
+                        // Apply margin to the right of the mentor view if it's the last mentor
+                        if (index == totalMentors - 1 || index == 4) {
+                            val params = mentorView.layoutParams as LinearLayout.LayoutParams
+                            params.rightMargin = resources.getDimensionPixelSize(R.dimen.margin_end)
+                            mentorView.layoutParams = params
+                        }
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors
+            }
+        })
+
 //        val notifsBtn = findViewById<ImageButton>(R.id.notifs_btn)
         binding.notifsBtn.setOnClickListener{
             startActivity(Intent(this, Notifications::class.java))
-        }
-
-//        val jcBtn = findViewById<TextView>(R.id.jc_txt)
-        binding.jcTxt.setOnClickListener{
-            startActivity(Intent(this, MentorPg::class.java))
         }
 
         //////////////////BOTTOM NAV BAR////////////////////
---
+++ i202606/Mentor.kt
@@ -0,0 +1,4 @@
+package com.maliha.i202606
+
+data class Mentor(val mentorname : String? = null, val name: String? = null, val desc: String? = null,
+                  val price: Int? = null, val status: String? = null, val type: String? = null, val fav: Boolean = true)
